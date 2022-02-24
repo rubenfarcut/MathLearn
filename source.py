3d36a8dd27d4b00439dcb17d4fb5c886a57f42f2
rubenfarcut <rubenfarcut@gmail.com>
2022-02-24 20:39:07 +0200

    Add menu_bar on top with button of exit

--- source.py
+++ source.py
@@ -1,7 +1,24 @@
+from distutils import command
 from tkinter import *
+
+
+
 window =Tk()
 window.attributes("-fullscreen",TRUE)
 window.title("MathLearn.exe")
 #window.configure(width=500,height=300)
 window.configure(bg='#7FC1F9')
+
+menu=Menu(window)
+window.config(menu=menu)
+
+filemenu=Menu(menu,font="Arial 15", background='#2E64FE',
+ foreground='white',activebackground='aqua',
+  activeforeground='black',relief=GROOVE,tearoff=0)
+menu.add_cascade(label="Fișier",menu=filemenu)
+
+filemenu.add_command(label="Închidere",command=window.destroy)
+
+
+
 window.mainloop()
