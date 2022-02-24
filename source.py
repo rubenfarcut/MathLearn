a51ded6583373d244d6bfcedd6df0623b0a86c5d
rubenfarcut <rubenfarcut@gmail.com>
2022-02-24 18:37:05 +0200

    Create a window in fullscreen mode

--- source.py
+++ source.py
@@ -1,1 +1,7 @@
-print("Hello world!")
+from tkinter import *
+window =Tk()
+window.attributes("-fullscreen",TRUE)
+window.title("MathLearn.exe")
+#window.configure(width=500,height=300)
+window.configure(bg='#7FC1F9')
+window.mainloop()
